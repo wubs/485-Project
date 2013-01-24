924169021a8cbb7a83b19856e9c9598c90417ee5
wubs <wubs@eecs485-08.eecs.umich.edu>
2013-01-24 17:12:17 -0500

    wubs upload

--- eecs485-pa1/html/editalbumlist.php
+++ eecs485-pa1/html/editalbumlist.php
@@ -18,21 +18,27 @@
           </tr>
         </thead>
 
-        <tbody>
+        <tbody id="table_body" >
           <?php 
             $conn = mysql_connect($db_host, $db_user, $db_passwd)
             or die("Connect Error: " . mysql_error());
             
             mysql_select_db($db_name) or die("Could not select:" . $db_name);
             
-            $query = "SELECT title,access FROM Album WHERE access='public'";
+            $query = "SELECT title,access,albumid FROM Album WHERE access='public'";
             $result = mysql_query($query) or die("Query failed: " . mysql_error());
             
             while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
               echo "<tr> <td>" . $line['title'] . "</td>"
                 . "<td>" . $line['access'] . "</td>"
-                 . "<td><a class='btn btn-primary'>Edit</a></td>"
-                 . "<td><a class='btn btn-danger'>Del</a></td></tr>";
+                . "<td><a class='btn btn-primary'>Edit</a></td>"
+                . "<td><a class='btn btn-danger Del' albumid='". $line['albumid'] . "'>Del</a></td>";
+                /*
+                . "<form action='deletealbum.php' method='post'>"
+                . "<input name='op' type='hidden' value='delete'>"
+                . "<input name='albumid' type='hidden' value=".$line['albumid'].">"
+                . "<input class='btn btn-danger' type='submit' value='Del'></form>";
+                */
             }
             
             mysql_free_result($result);
@@ -55,10 +61,40 @@
             <input name="op" type="hidden" value="add">
             <input class='btn btn-success' type="submit" value="Add">
           </form>
+
+
     <!-- edit above -->
     </div> <!-- /container -->
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     <script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.2.2/js/bootstrap.min.js"></script>
-  </body>
+    
+    <script type="text/javascript">
+      $(function () { ///
+
+        $(".Del").live("click", function() {     
+          var albumid = $(this).attr('albumid');
+          $.post("deletealbum.php", {"op": 'delete', "albumid": albumid }, function(data) {
+            location.reload();
+            // refresh 
+              // data: [data_item: {title, access albumid}, ...  ]
+             // $("#table_body").empty();
+
+             // for (i=0; i<data.length; i++) {
+             //   var ttitle = data[i].title;
+             //   var taccess = data[i].access;
+             //   var tid = data[i].albumid;
+             //   var tr = "<tr> <td>" + ttitle +  "</td><td>" + taccess + "</td><td><a class='btn btn-primary'>Edit</a></td><td><a class='btn btn-danger Del' albumid='" + tid + "'>Del</a></td>";
+             //   $("#table_body").append(tr);
+             // }
+              
+              
+            });   
+        });
+
+
+     }); ///
+    </script>
+
+</body>
 </html>
