e86aa716b9273570d369af5ddf2697cd6a154e3f
Ruoran Wang <ruoranwang@gmail.com>
2013-01-28 18:19:33 -0500

    upload almost done

--- eecs485-pa1/html/upload.php
+++ eecs485-pa1/html/upload.php
@@ -10,10 +10,27 @@
   $caption = $_POST['caption'];
   $image_name = $_FILES["file"]["name"];
   $image_type = $_FILES["file"]["type"];
-  //list($base, $image_type) = $image_type.split("/", $image_type);
+  $pieces = explode("/",  $image_type);
+  $image_type = strtolower($pieces[1]);
+
+  $supported_ext = array("jpg", "gif", "png", "bmp", "tif");
+
+  if ($image_type == "jpeg" ) {
+    $image_type == "jpg";
+  } else if ( !in_array($image_type, $supported_ext) ) {
+    echo "Image type not supported";
+    return;
+  } 
 
   $url = "static/images/$image_name";
-  $image_data = chunk_split(base64_encode(file_get_contents($_FILES["file"]["name"])));
+  $image_data = base64_encode(file_get_contents($_FILES["file"]["tmp_name"]));
+
+//  echo " image name " . $image_name;
+//  echo " image type " . $image_type;
+//  echo " aid " . $albumid;
+//  echo " url " . $url;
+//  echo " caption " . $caption;
+//  echo " image_data:" . base64_encode(file_get_contents($_FILES["file"]["tmp_name"]));
 
   $query = "SELECT url FROM Photo WHERE url='$url' LIMIT 1";
   $result = mysql_query($query) or die(mysql_error());
@@ -21,23 +38,7 @@
 
   // Check url existence  // Check Photo existence
   if ($raw['url'] == null) {
-    echo " yes ";
-    $query = "INSERT INTO Photo (url, code, format, date) values ('$url', '$image_data', '$image_type', NOW())";
-    $result = mysql_query($query) or die("Query failed: " . mysql_error());
-  }
-
-  echo " image name " . $image_name;
-  echo " immage type " . $image_type;
-  echo " aid " . $albumid;
-  echo " url " . $url;
-  echo " caption " . $caption;
-  echo " image_data:" . file_get_contents($_FILES["file"]["tmp_name"]);
-
-  $query = "SELECT url FROM Contain WHERE url='$url' AND albumid=$albumid  LIMIT 1";
-  $result = mysql_query($query) or die(mysql_error());
-  $raw = mysql_fetch_array($result, MYSQL_ASSOC);
-  if ($raw['url'] == null) {
-    echo "new in contain";
+    echo "<p>Added new photo</p>";
     $query = "INSERT INTO Photo (url, code, format, date) values ('$url', '$image_data', '$image_type', NOW())";
     $result = mysql_query($query) or die("Query failed: " . mysql_error());
   }
@@ -47,8 +48,16 @@
   $raw = mysql_fetch_array($result, MYSQL_ASSOC);
   $new_seq = intval($raw['sequencenum']) + 1;
 
-//  $query = "INSERT INTO Contain (albumid, url, caption, sequencenum) values ($albumid, '$url', '$caption', $new_seq)";
-//  $result = mysql_query($query) or die("Query failed: " . mysql_error());
+  $query = "SELECT url FROM Contain WHERE url='$url' AND albumid=$albumid  LIMIT 1";
+  $result = mysql_query($query) or die(mysql_error());
+  $raw = mysql_fetch_array($result, MYSQL_ASSOC);
+  if ($raw['url'] == null) {
+    echo "<p>Added photo to album</p>";
+    $query = "INSERT INTO Contain (albumid, url, caption, sequencenum) values ($albumid, '$url', '$caption', $new_seq)";
+    $result = mysql_query($query) or die("Query failed: " . mysql_error());
+  } else {
+    echo "<p> Photo already exists in this album </p>";
+  }
 
   mysql_free_result($result);
   mysql_close($conn);
