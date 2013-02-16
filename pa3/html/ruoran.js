8f63799eec3204340a4fca0edd48a70890bf692b
Ruoran Wang <ruoranwang@gmail.com>
2013-02-16 12:22:22 -0500

    drag and drop

--- pa3/html/ruoran.js
+++ pa3/html/ruoran.js
@@ -1,7 +1,21 @@
 document.onmousedown = mouse_down;
 document.onmouseup = mouse_up;
 
+document.onmouseover = function(e) {
+  if (e.target.className == 'dest') {
+    over_dest = e.target;
+    console.log('over');
+  }
+};
+document.onmouseout = function(e) {
+  if (e.target.className == 'dest') {
+    over_dest = null;
+    console.log('out');
+  }
+};
+
 function position_to_int(pos) {
+  // use this to strip px out
   var i = parseInt(pos.replace('px', ''));
   if (i) {
     return i;
@@ -9,6 +23,26 @@ function position_to_int(pos) {
     return 0;
   }
 }
+
+//
+// fly is the element that is being dragged.
+//
+
+over_dest = null;
+
+register_dests();
+
+function register_dests() {
+  var dests = document.getElementsByClassName('dest');
+
+  for (var i = 0; i < dests.length; i++) {
+    var el = dests[i];
+    //el.onmouseover = function(e) {over_dest = e.target; console.log('over');};
+    //el.onmouseout = function(e) {over_dest = null; console.log('out');};
+    el.style.zIndex = 500;
+  }
+}
+
 
 function mouse_down(e) {
   e = e || window.event;
@@ -18,12 +52,18 @@ function mouse_down(e) {
 
   fly = e.target;
 
+  if (fly.className != "drag") {
+    fly = null;
+    return false;
+  }
+
   offset_x = position_to_int(fly.style.left);
   offset_y = position_to_int(fly.style.top);
 
   old_zIndex = fly.style.zIndex;
 
-  fly.style.zIndex = 100000;
+  fly.style.pointerEvents = "none";
+  fly.style.zIndex = 1;
 
   document.onmousemove = mouse_move;
   document.body.focus();
@@ -38,7 +78,13 @@ function mouse_move(e) {
 }
 
 function mouse_up(e) {
+  e = e || window.event;
+  console.log(e.target);
   if (fly != null) {
+    console.log(over_dest);
+    if (over_dest) {
+      // right p
+    } 
     step = restore(e.clientX + offset_x - down_x, e.clientY + offset_y - down_y);
     int_step = setInterval(step, 0.5);
   }
@@ -54,7 +100,6 @@ function restore(x, y) {
   this.count = 0
 
   var move_back = function () {
-    console.log('step');
     if (count < this.period) {
       this.cur_x -= this.unit_x; 
       this.cur_y -= this.unit_y; 
@@ -63,6 +108,7 @@ function restore(x, y) {
       this.count++;
     } else {
       fly.style.zIndex = old_zIndex;
+      fly.style.pointerEvents = null;
       fly.style.left = offset_x + 'px';
       fly.style.top = offset_y + 'px';
       document.onmousemove = null;
@@ -73,5 +119,3 @@ function restore(x, y) {
 
   return move_back;
 }
-
-
