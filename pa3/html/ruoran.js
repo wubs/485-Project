95222c104094cff9f65176396dba3c0e3fc578cd
Ruoran Wang <ruoranwang@gmail.com>
2013-02-18 00:48:20 -0500

    transparent drag

--- pa3/html/ruoran.js
+++ pa3/html/ruoran.js
@@ -1,19 +1,30 @@
+
+// main 
 document.onmousedown = mouse_down;
 document.onmouseup = mouse_up;
 
 document.onmouseover = function(e) {
   if (e.target.className == 'dest') {
     over_dest = e.target;
-    console.log('over');
-  }
-};
-document.onmouseout = function(e) {
-  if (e.target.className == 'dest') {
-    over_dest = null;
-    console.log('out');
+    //console.log('over');
   }
 };
 
+over_dest = null;
+
+register_dests();
+
+// end of main
+
+document.onmouseout = function(e) {
+  if (e.target.className == 'dest') {
+    over_dest = null;
+    //console.log('out');
+  }
+};
+
+
+// helper function
 function position_to_int(pos) {
   // use this to strip px out
   var i = parseInt(pos.replace('px', ''));
@@ -28,10 +39,6 @@ function position_to_int(pos) {
 // fly is the element that is being dragged.
 //
 
-over_dest = null;
-
-register_dests();
-
 function register_dests() {
   var dests = document.getElementsByClassName('dest');
 
@@ -42,7 +49,6 @@ function register_dests() {
     el.style.zIndex = 500;
   }
 }
-
 
 function mouse_down(e) {
   e = e || window.event;
@@ -57,12 +63,24 @@ function mouse_down(e) {
     return false;
   }
 
+  semi = fly.nextSibling.nextSibling;
+
   offset_x = position_to_int(fly.style.left);
   offset_y = position_to_int(fly.style.top);
 
+  semi.style.display = "inline";
+  semi.style.opacity = "0.8";
+  fly.style.opacity = "0.2";
+
   old_zIndex = fly.style.zIndex;
 
+  old_fly = fly;
+  fly = semi;
+
   fly.style.pointerEvents = "none";
+  // since fly element is always below the cursor
+  // we have to disable pointerEvents for fly
+  // otherwise the event will not happen on the destination
   fly.style.zIndex = 1;
 
   document.onmousemove = mouse_move;
@@ -79,20 +97,34 @@ function mouse_move(e) {
 
 function mouse_up(e) {
   e = e || window.event;
-  console.log(e.target);
   if (fly != null) {
-    console.log(over_dest);
     if (over_dest) {
-      // right p
+      // user dropped the element at
+      // right location
+      console.log(e.target);
+      console.log(over_dest);
+
+
+      // trigger ajax to grand user access to album
+      //
+      // username = over_dest.value()
+      // albumid = fly.value()
+
     } 
+
+    // created a closure
     step = restore(e.clientX + offset_x - down_x, e.clientY + offset_y - down_y);
+
+    // this will move the block back to origin pos
+    // setInterval will stop by step() if it is done
     int_step = setInterval(step, 0.5);
   }
 }
 
+// restore is more like a class
 function restore(x, y) {
 
-  this.period = 30.0;
+  this.period = 30.0; // how many steps
   this.cur_x = x;
   this.cur_y = y;
   this.unit_x = (x - offset_x) / this.period; 
@@ -107,15 +139,21 @@ function restore(x, y) {
       fly.style.top = this.cur_y + 'px';
       this.count++;
     } else {
+      // done, restore fly attributes
       fly.style.zIndex = old_zIndex;
       fly.style.pointerEvents = null;
       fly.style.left = offset_x + 'px';
       fly.style.top = offset_y + 'px';
       document.onmousemove = null;
+      semi.style.display = "none";
+      semi.style.opacity = "0.0";
+      old_fly.style.opacity = "1";
       fly = null;
+      // stop the interval loop
       clearInterval(int_step);
     }
   }
 
+  // return the handle
   return move_back;
 }
