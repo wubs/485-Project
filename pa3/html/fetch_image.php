92aff57806368ff3b0778ccaa4cdabb3a472bf07
Ruoran Wang <ruoranwang@gmail.com>
2013-02-22 22:37:22 -0500

    fetch image

---
+++ pa3/html/fetch_image.php
@@ -0,0 +1,23 @@
+<?php 
+  include('lib.php'); 
+  $url = $_POST['url']; 
+
+  $conn = mysql_connect($db_host, $db_user, $db_passwd)
+  or die("Connect Error: " . mysql_error());
+  
+  mysql_select_db($db_name) or die("Could not select:" . $db_name);
+
+  $query = "select format, code from Photo where url='$url';";
+
+  $result = mysql_query($query) or die(mysql_error());
+
+  $photo = mysql_fetch_array($result, MYSQL_ASSOC);
+
+  // $base64
+  // front-end src=$base64 
+
+
+  echo json_encode($base64);
+
+  mysql_close($conn);
+?>
