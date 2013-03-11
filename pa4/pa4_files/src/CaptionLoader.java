40f941a7e417ce78038484b11547816fc9f30702
Ruoran Wang <ruoranwang@gmail.com>
2013-03-10 20:46:41 -0400

    CaptionLoader done

---
+++ pa4/pa4_files/src/CaptionLoader.java
@@ -0,0 +1,91 @@
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.Node;
+import org.w3c.dom.Element;
+import java.io.File;
+
+import java.sql.*;
+
+public class CaptionLoader {
+    static File fXmlFile = null;
+    static String db_name = "pa1_db";
+    static String db_user = "ruoran";
+    static String db_pass = "1216";
+    
+    public static void main(String [] args) {
+        
+        try {
+            System.out.println("Start loading caption");
+            fXmlFile = new File("search.xml");
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1); 
+        }
+
+        try {
+            System.out.println("Connecting DB");
+            String db_url = "jdbc:mysql://localhost:3306/" + db_name;
+            Connection conn = null;
+            Statement statement = null;
+            int updateQuery = 0;
+
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+
+            conn = DriverManager.getConnection(db_url, db_user, db_pass);
+
+            statement = conn.createStatement();
+            
+            // Create Album
+            
+            String queryString = "INSERT IGNORE Album (albumid, title, created, lastupdated, access, username) values" +
+                    "(5, 'PA4 Album', NOW(), NOW(), 'public', 'traveler');";
+            System.out.println(queryString);
+            updateQuery = statement.executeUpdate(queryString);
+            
+            
+            // load Photo table
+
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(fXmlFile);
+
+            doc.getDocumentElement().normalize();
+
+            NodeList nList = doc.getElementsByTagName("photo");
+            for (int i=0; i<nList.getLength(); i++) {
+                Node nNode = nList.item(i);
+                if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+                    Element el = (Element) nNode;
+
+                    String url = "static/images/" + el.getAttribute("filename");
+                    String code = "empty";
+                    String format = "jpg";
+                    String date = "NOW()";
+                    String caption = el.getAttribute("caption");
+                    String seq = el.getAttribute("sequencenum");
+                        
+                    // Load Photo table
+                    queryString = "INSERT IGNORE Photo (url, code, format, date) VALUES " +
+                            "('" + url + "','" + code + "','" + format + "'," + date + ")"; 
+
+                    System.out.println(queryString);
+                    updateQuery = statement.executeUpdate(queryString);
+                   
+                    // Load Contain table
+                    queryString = "INSERT IGNORE Contain (albumid, url, caption, sequencenum) VALUES " +
+                            "(5, '" + url + "',\"" + caption + "\"," + seq + ")"; 
+
+                    System.out.println(queryString);
+                    updateQuery = statement.executeUpdate(queryString);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1); 
+        }
+    }
+}
